7887caa1c07a39e82af1c7021e51aea0b544cac5
Day-C <kongranalanyuy@gmail.com>
2024-05-06 17:54:11 -0700

    generate a .tgz archive file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''Fabric script to generate a .tgz archive'''
 from datetime import datetime
-from fabric.api import *
+from fabric import Connection
 
 
 def do_pack():
@@ -9,12 +9,12 @@ def do_pack():
 
 
     now = datetime.now()
-
-    archive_name = "web_static_" + now.strtftime('%Y%m%d%H%M%S') + '.' + '.tgz'
-    local('mkdir -p versions')
-    # Create and a .tgz archive file
-    create = local('tar  -czvf versions/{} web_static'.format(archive_name))
-    if create is not None:
-        return create
-    else:
-        return None
+    with Connection(host='54.144.137.84', user='ubuntu') as c:
+        archive_name = "web_static_" + now.strtftime('%Y%m%d%H%M%S') + '.' + '.tgz'
+        local('mkdir -p versions')
+        # Create and a .tgz archive file
+        create = c.local('tar  -czvf versions/{} web_static'.format(archive_name))
+        if create is not None:
+            return create
+        else:
+            return None
