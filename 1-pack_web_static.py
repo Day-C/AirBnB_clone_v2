eb340acfd654079e0aaab7c3cb6efd9053648e91
Day-C <kongranalanyuy@gmail.com>
2024-05-06 18:26:59 -0700

    generate archive file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-'''Fabric script to generate a .tgz archive'''
+'''Fabric script to generate a .tgz archive.'''
 from datetime import datetime
 from fabric import Connection
 
 
 def do_pack():
-    '''Do_pack compreses the content of the web static  folder into a tar achive'''
+    '''Do_pack compreses the content folder into a tar achive.'''
 
-    now = datetime.now()
     with Connection(host='54.144.137.84', user='ubuntu') as c:
-        archive_name = "web_static_" + now.strtftime('%Y%m%d%H%M%S') + '.' + '.tgz'
+        now = datetime.now()
+        txt = "web_static_" + now.strtftime('%Y%m%d%H%M%S') + '.' + '.tgz'
+        archive_name = txt
         local('mkdir -p versions')
         # Create and a .tgz archive file
-        create = c.local('tar  -czvf versions/{} web_static'.format(archive_name))
-        if create is not None:
+        crt = c.local('tar  -czvf versions/{} web_static'.format(archive_name))
+        if crt is not None:
             return create
         else:
             return None
