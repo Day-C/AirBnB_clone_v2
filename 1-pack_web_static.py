101f67bdbf60ad816e6f368437c5e4331a950b6e
Day-C <kongranalanyuy@gmail.com>
2024-05-06 17:56:43 -0700

    generate a .tgz archive file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,7 +7,6 @@ from fabric import Connection
 def do_pack():
     '''Do_pack compreses the content of the web static  folder into a tar achive'''
 
-
     now = datetime.now()
     with Connection(host='54.144.137.84', user='ubuntu') as c:
         archive_name = "web_static_" + now.strtftime('%Y%m%d%H%M%S') + '.' + '.tgz'
