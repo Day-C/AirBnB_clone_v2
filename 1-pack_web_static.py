271100628073159be747b8ee776412454ab96b1f
Day-C <kongranalanyuy@gmail.com>
2024-05-13 05:19:14 -0700

    test01

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,14 +8,6 @@ def do_pack():
     """
     Do_pack compreses the content folder into a tar achive.
     """
-
-    with Connection(host='54.144.137.84', user='ubuntu') as c:
-        now = datetime.now()
-        txt = "web_static_" + now.strtftime('%Y%m%d%H%M%S') + '.' + '.tgz'
-        archive_name = txt
-        local('mkdir -p versions')
-        # Create and a .tgz archive file
-        crt = c.local('tar  -czvf versions/{} web_static'.format(archive_name))
         if crt is not None:
             return create
         else:
