19f1acf3edd3831f51d6992db549c50c840702b0
Day-C <kongranalanyuy@gmail.com>
2024-05-13 06:01:40 -0700

    is clear

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,11 +5,11 @@ from datetime import datetime
 from fabric import Connection
 
 
-def do_pack():
+def do_pack(crt):
     """
     Do_pack compreses the content folder into a tar achive.
     """
-        if crt is not None:
-            return create
-        else:
-            return None
+    if crt:
+        return create
+    else:
+        return None
