b8bddb891dbd3afbe3cabace439bb7bcf6e12f62
Day-C <kongranalanyuy@gmail.com>
2024-05-19 15:22:25 -0700

    display  a simple string

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''Start a flask application with multiple routes.'''
+from flask import Flask
+
+
+app = Flask(__name__)
+app.config['STATIC_SLASHES'] = False
+
+
+@app.route('/')
+def hello():
+    '''Display greeting.'''
+
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hbnb():
+    '''Display a nmae'''
+
+    return 'HBNB'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
